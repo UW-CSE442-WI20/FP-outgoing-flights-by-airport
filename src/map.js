80759777cb550c7a5a398019ca7f861bc593b0c6
khaittran <erick84khai@gmail.com>
2020-03-13 13:39:37 -0700

    add tooltip for chart

--- src/map.js
+++ src/map.js
@@ -77,7 +77,7 @@ d3.csv(csvData).then(function(data) {
         return row.Origin == parsed.airline
     }));
 
-    var select = d3.select(".dropdown2")
+    var select = d3.select(".dropdown1")
       .append("div")
       .append("select")
 
@@ -99,11 +99,41 @@ d3.csv(csvData).then(function(data) {
       .data(types)
       .enter()
         .append("option")
-        .attr("class", "dropdown2")
+        .attr("class", "dropdown1")
         .attr("value", function (d) { return d; })
         .text(function (d) { return toProperCase(d); });
 });
 
+d3.csv(getFile, function(d){
+    return {
+    type : d.AirPort,
+    code : d.AirID
+  };
+  }).then(function(data) {
+    for(var i = 0; i < data.length; i++){
+      var id = data[i].code;
+      ID_Name.set(id, data[i].type);
+    }
+  });
+
+function distinct_Types(rows) {
+  for(var i = 0; i < rows.length; i++) {
+    types[i] = ID_Name.get(rows[i]);
+  }
+  types = [...new Set(types)].sort();
+  types.unshift("Please Select an AirPort");
+  //console.log(types)
+  return types;
+}
+
+function toProperCase(value) {
+  var words = value.split(" ");
+  var result = words[0].substring(0, 1).toUpperCase() + words[0].substring(1, words[0].length).toLowerCase(); 
+  for (var i = 1; i < words.length; i++) {
+    result += " " + words[i].substring(0, 1).toUpperCase() + words[i].substring(1, words[i].length).toLowerCase(); 
+  }
+  return result;
+}
 function draw_data(data){
 
     console.log(data);
@@ -217,38 +247,6 @@ function draw_data(data){
  info.addTo(map);
 
 
-
-d3.csv(getFile, function(d){
-      return {
-      type : d.AirPort,
-      code : d.AirID
-    };
-    }).then(function(data) {
-      for(var i = 0; i < data.length; i++){
-        var id = data[i].code;
-        ID_Name.set(id, data[i].type);
-      }
-    });
-
-function distinct_Types(rows) {
-    for(var i = 0; i < rows.length; i++) {
-      types[i] = ID_Name.get(rows[i]);
-    }
-    types = [...new Set(types)].sort();
-    types.unshift("Please Select an AirPort");
-    console.log(types)
-    return types;
-}
-
-function toProperCase(value) {
-    var words = value.split(" ");
-    var result = words[0].substring(0, 1).toUpperCase() + words[0].substring(1, words[0].length).toLowerCase(); 
-    for (var i = 1; i < words.length; i++) {
-      result += " " + words[i].substring(0, 1).toUpperCase() + words[i].substring(1, words[i].length).toLowerCase(); 
-    }
-    return result;
-}
-
 // --------------------------
 // DISPLAY SUMMARY INFO BELOW
 // --------------------------
@@ -313,4 +311,100 @@ function tallyData() {
     });
 }
 
-// --------------------------
+// ------------------------
+// The bar chart
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x = d3.scaleBand()
+          .range([0, width])
+          .padding(0.1);
+var y = d3.scaleLinear()
+          .range([height, 0]);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", 
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Default will be set to january flights, need to change file
+// based on selected month
+const JanData = require('../static/jan.csv');
+d3.csv(JanData).then(function(data) {
+    data.forEach(function(d) {
+        d.Origin = d.Origin;
+        d.Dest = d.Dest
+        d.Date = d.Date
+        //console.log(d.Date)
+        d.Distance = +d.Distance
+        d.Count = +d.Count    
+    });
+    
+  const results = data.filter(function (row) {
+    return (row.Origin == 'SEA' && row.Dest == 'ORD') // Need to change ariports based on selection
+  });
+
+  //console.log(results);
+  x.domain(results.map(function(d) { return d.Date; }));
+  y.domain([0, d3.max(results, function(d) { return d.Count; })]);
+
+  // add bars
+  svg.selectAll("#bar")
+      .data(results)
+      .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.Date); })
+      .attr("width", x.bandwidth())
+      .attr("y", function(d) { return y(d.Count); })
+      .attr("height", function(d) { return height - y(d.Count); })
+      .on("mouseover", handleMouseOver)
+      .on("mouseout", handleMouseOut)
+
+  //x axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x).ticks(10))
+      .selectAll("text")	
+        .style("text-anchor", "end")
+        .attr("transform", "rotate(-65)");
+
+  // y axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
+
+});
+// define feagure div for tip
+var div = d3.select("body").append("div")	
+    .attr("class", "tooltip")				
+    .style("opacity", 0);
+
+// mouse handle the 
+function handleMouseOver(d, i) {
+  toolTipMap(`Name of Airport level ${d.Count} in date: ${d.Date}`);
+  // Use D3 to select element, change color and size
+  d3.select(this).style("opacity", .7);
+
+}
+
+function handleMouseOut(d, i) { 
+  // Use D3 to select element, change color and size
+  //console.log("mouse", this);
+  div.transition()		
+                .duration(500)		
+                .style("opacity", 0);	
+  d3.select(this).style("opacity", 1);
+}
+
+// mouse click
+function toolTipMap(d){
+  div.transition()    
+        .duration(150)    
+        .style("opacity", .9);
+      div.html(d) 
+        .style("left", (d3.event.pageX) + "px")   
+        .style("top", (d3.event.pageY - 28) + "px");
+}
+
